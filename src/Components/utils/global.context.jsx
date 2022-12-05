0c54af697535e162c3c0313f4cfb648e485f9852
Damián Cano López <damiancano1811@hotmail.com>
2022-12-05 18:00:07 -0500

    trabajo listo, ahora solo toca retocar los styles

--- src/Components/utils/global.context.jsx
+++ src/Components/utils/global.context.jsx
@@ -1,45 +1,63 @@
-import axios from 'axios'
-import { useReducer } from 'react';
+import axios from "axios";
+import { useReducer } from "react";
 import { useEffect, useState, createContext, useContext } from "react";
 
-export const initialState = {theme: "dark"}
+export const initialState = { theme: "light" }; //Esto se usa si lo hacemos con useReducer (creo jeje)
 
 const reducer = (state, action) => {
-  switch (action.type){
+  switch (action.type) {
     case "theme":
-      return {theme: state.theme === "light" ? "dark" : "light"};
+      return { theme: state.theme === "light" ? "dark" : "light" };
 
-      default:
-        throw new Error();
+    default:
+      throw new Error();
   }
-}
+};
 
 export const ContextGlobal = createContext();
 
 const ContextProvider = ({ children }) => {
   //Aqui deberan implementar la logica propia del Context, utilizando el hook useMemo
-  
-  const [theme, dispatch] = useReducer(reducer, initialState);
-  const [dentist, setDentist] = useState([])
 
+  const [theme, dispatch] = useReducer(reducer, initialState); //Esto lo agregue para hacer el theme con useState, VER!;
+  const [dentist, setDentist] = useState([]);
+  const [favs, setFavs] = useState([])
+  
+
+  const KEY = "card.info"
+
+  useEffect(() => {
+      const storedFavs = JSON.parse(localStorage.getItem(KEY));
+      if (storedFavs) {
+          setFavs(storedFavs)
+      }
+  }, [])
+  
+  useEffect(() => {
+      localStorage.setItem(KEY, JSON.stringify(favs))
+  }, [favs])
 
 
   const getDentist = () => {
-    axios.get("https://jsonplaceholder.typicode.com/users")
-    .then( res => setDentist(res.data) )
-  }
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setDentist(res.data));
+  };
 
   useEffect(() => {
-    getDentist()
-  }, [] )
-  
+    getDentist();
+  }, []);
 
   return (
-    <ContextGlobal.Provider value={{
-      dentist,
-      theme,
-      dispatch,
-    }}>
+    <ContextGlobal.Provider
+      value={{
+        dentist,
+        theme,
+        dispatch,
+        favs,
+        setFavs
+      }}
+    >
       {children}
     </ContextGlobal.Provider>
   );
@@ -48,5 +66,5 @@ const ContextProvider = ({ children }) => {
 export default ContextProvider;
 
 export const useContextGlobal = () => {
-  return useContext(ContextGlobal)
-}
+  return useContext(ContextGlobal);
+};
