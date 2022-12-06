4f6dc87a316bb86042239717840daccd61ce09c7
ANDRES <santibeta133@gmail.com>
2022-12-06 13:07:35 -0500

    modo oscuro finalizado

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,12 +3,12 @@ import { useContextGlobal } from './utils/global.context'
 
 const Footer = () => {
   const {theme} = useContextGlobal();
-
+  console.log(theme.theme);
 
   return (
     <footer className='footer' id={theme.theme}>
-        <p>Powered by</p>
-        <img src="./images/DH.png" alt='DH-logo'/>
+        <p>Powe"red by</p>
+        {theme.theme === "light" ? <img src="./images/DH.png" alt='DH-logo'/> : <img src="./images/DH2.png" alt='DH2-logo'/>}
     </footer>
   )
 }
